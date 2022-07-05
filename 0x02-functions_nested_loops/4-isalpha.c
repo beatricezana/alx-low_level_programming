58a54c1e1e5c4f02cd1af7ebd8d96fb2a6515ab3
beatricezana <beatricezana@gmail.com>
2022-07-05 14:12:30 -0700

    fa

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * _isalpha - tests whether a character is from the English alphabet.
  * @c: character to be checked.
@@ -8,9 +8,7 @@
 int _isalpha(int c)
 {
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-
 return (1);
 else
 return (0);
-
 }
