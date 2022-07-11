058bdb1d7e23e15a66323fde1a4d4f3f78cdc42f
beatricezana <beatricezana@gmail.com>
2022-07-11 09:58:47 -0700

    op

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,28 +1,18 @@
 #include "main.h"
 /**
- * rev_string - reverses a string
- * @s: holi
+ * string_toupper - changes all lowercase letters to uppercase
+ * @s: string to change
+ * Return: address of s
  */
-void rev_string(char *s)
+char *string_toupper(char *s)
 {
-	int i;
-	int j;
-	int temp;
-	int length;
+int i = 0;
 
-	while (s[i] != 0)
-	{
-		i++;
-	}
-	length = i;
-	i = 0;
-	j = length - 1;
-	while (i < j)
-	{
-		temp = s[i];
-		s[i] = s[j];
-		s[j] = temp;
-		i++;
-		j--;
-	}
+while (*(s + i))
+{
+	if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		*(s + i) -= 'a' - 'A';
+	i++;
+}
+return (s);
 }
