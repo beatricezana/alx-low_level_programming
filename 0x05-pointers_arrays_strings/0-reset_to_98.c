afcef0219d4cebf59a9b05b626b29ab2df4bd932
Christopher Akinsanmi <Christopher.akinsanmi@gmail.com>
2022-07-13 10:40:07 -0700

    0. 98 Battery st. :Write a function that takes a pointer to an int as parameter and updates the value it points to to 98. Prototype: void reset_to_98(int *n);

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,19 @@
-#include "main.h"
+#include <stdio.h>
+
+void reset_to_98(int *);
 
 /**
- * reset_to_98 - takes a pointer to an int as parameter
- * and update the value it point to 98
- * @n: parameter take the value.
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
-void reset_to_98(int *n)
+int main(void)
 {
-*n = 98;
+int n;
+
+n = 402;
+printf("%d\n", n);
+reset_to_98(&n);
+printf("%d\n", n);
+return (0);
 }
